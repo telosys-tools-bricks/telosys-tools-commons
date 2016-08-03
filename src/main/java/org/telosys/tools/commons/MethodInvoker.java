b32d4c610de673c7e3c2e59d65929f8df016f714
Laurent Guerin <laguerin@LFR002634.corp.capgemini.com>
2016-08-03 17:58:40 +0200

    MethodInvoker class for #checkId directive

---
+++ src/main/java/org/telosys/tools/commons/MethodInvoker.java
@@ -0,0 +1,81 @@
+/**
+ *  Copyright (C) 2008-2015  Telosys project org. ( http://www.telosys.org/ )
+ *
+ *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.gnu.org/licenses/lgpl.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.telosys.tools.commons;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+/**
+ * @author Laurent GUERIN
+ *
+ */
+public class MethodInvoker
+{
+	private final static String ERROR_GET_GETTER_METHOD    = "Cannot get method " ;
+	private final static String ERROR_INVOKE_GETTER_METHOD = "Cannot invoke method " ;
+
+	/**
+	 * Full static class : no public constructor
+	 */
+	private MethodInvoker() {
+	}
+
+	//----------------------------------------------------------------------------------
+	public static Object invokeGetter(Object instance, String methodName) {
+
+		if ( null == instance ) {
+			throw new IllegalArgumentException("Object instance argument is null");
+		}
+		if ( null == methodName ) {
+			throw new IllegalArgumentException("Method name argument is null");
+		}
+
+		Class<?> cl = instance.getClass();
+		
+		Object result = null ;
+		Method method ;
+		try {
+			method = cl.getMethod(methodName, (Class<?>[])null); 
+		} catch (SecurityException e) {
+			throw new RuntimeException(ERROR_GET_GETTER_METHOD + methodName + " (SecurityException)", e);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(ERROR_GET_GETTER_METHOD + methodName + " (NoSuchMethodException)", e);
+		}
+
+		try {
+			result = method.invoke(instance, (Object[])null);
+		} catch (IllegalArgumentException e) {
+			throw new RuntimeException(ERROR_INVOKE_GETTER_METHOD + methodName + " (IllegalArgumentException)", e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(ERROR_INVOKE_GETTER_METHOD + methodName + " (IllegalAccessException)", e);
+		} catch (InvocationTargetException e) {
+			throw new RuntimeException(ERROR_INVOKE_GETTER_METHOD + methodName + " (InvocationTargetException)", e);
+		}
+		return result ;
+	}
+	
+	public static boolean invokeBooleanGetter(Object instance, String methodName) {
+		Object result = invokeGetter(instance, methodName);
+		if ( result instanceof Boolean ) {
+			return (Boolean) result ;
+		}
+		else {
+			throw new RuntimeException("Invalid type returned by method " + methodName + " (boolean expected)" );
+		}
+	}
+	
+}
---
+++ src/test/java/org/telosys/tools/commons/MethodInvokerTest.java
@@ -0,0 +1,92 @@
+package org.telosys.tools.commons;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+public class MethodInvokerTest {
+
+	private class Person {
+		private int code ;
+		private String name ;
+		private boolean ok ;
+		
+		public Person(int code, String name, boolean ok) {
+			super();
+			this.code = code;
+			this.name = name;
+			this.ok = ok;
+		}
+		
+		public int getCode() {
+			return code;
+		}
+//		public void setCode(int code) {
+//			this.code = code;
+//		}
+		public String getName() {
+			return name;
+		}
+//		public void setName(String name) {
+//			this.name = name;
+//		}
+		public boolean isOk() {
+			return ok;
+		}
+//		public void setOk(boolean ok) {
+//			this.ok = ok;
+//		}
+		
+	}
+	
+	@Test
+	public void test1() {
+		Person p = new Person(1, "Bart", true);
+		Object code = MethodInvoker.invokeGetter(p, "getCode");
+		assertNotNull(code);
+		assertEquals(1, code);
+		assertEquals(p.getCode(), code);
+		
+		Object name = MethodInvoker.invokeGetter(p, "getName");
+		assertNotNull(name);
+		assertEquals("Bart", name);
+		assertEquals(p.getName(), name);
+		
+		Object ok   = MethodInvoker.invokeGetter(p, "isOk");
+		assertNotNull(ok);
+		assertEquals(p.isOk(), ok);
+	}
+
+	@Test
+	public void test2() {
+		Person p = new Person(2, "Foo", false);
+		Object code = MethodInvoker.invokeGetter(p, "getCode");
+		assertNotNull(code);
+		assertEquals(p.getCode(), code);
+		
+		Object name = MethodInvoker.invokeGetter(p, "getName");
+		assertNotNull(name);
+		assertEquals(p.getName(), name);
+		
+		Object ok   = MethodInvoker.invokeGetter(p, "isOk");
+		assertNotNull(ok);
+		assertEquals(p.isOk(), ok);
+	}
+
+	@Test
+	public void testBooleanGetter() {
+		Person p = new Person(1, "Bart", true);
+		boolean ok   = MethodInvoker.invokeBooleanGetter(p, "isOk");
+		System.out.println("ok = " + ok);
+		assertNotNull(ok);
+		assertEquals(ok, true);
+		assertTrue(ok);
+	}
+
+	@Test (expected=RuntimeException.class)
+	public void testBooleanGetterInvalid() {
+		Person p = new Person(1, "Bart", true);
+		MethodInvoker.invokeBooleanGetter(p, "isBlaBla");
+	}
+}
