14e08e4f7db3c992ff7997b7dcdd7c92bbfe1139
Laurent Guerin <laurent.guerin@sogeti.com>
2017-06-19 23:48:31 +0200

    new JUnit tests for VariablesManager

--- src/test/java/org/telosys/tools/commons/variables/VariablesManagerTest.java
+++ src/test/java/org/telosys/tools/commons/variables/VariablesManagerTest.java
@@ -34,19 +34,47 @@ public class VariablesManagerTest extends TestCase {
 //		VariablesManager variablesManager = new VariablesManager(hm) ;
 		VariablesManager variablesManager = getVariablesManagerInstance();
 		
+		// No variable(s) in the original string
 		replaceVariables(variablesManager, null, null);
 		replaceVariables(variablesManager, "",  "");
-		replaceVariables(variablesManager, "aa", "aa");
-		
+		replaceVariables(variablesManager, "aa", "aa");		
+		replaceVariables(variablesManager, "aabbcc",  "aabbcc");
+		replaceVariables(variablesManager, " a b ccc ddd", " a b ccc ddd");
+		replaceVariables(variablesManager, "     ",  "     ");
+		replaceVariables(variablesManager, "aaa${}zzz", "aaa${}zzz");
+
+		// 1 variable in the original string
 		replaceVariables(variablesManager, " aa ${VAR1} ",  " aa VALUE1 ");
 		replaceVariables(variablesManager, "${VAR1}",       "VALUE1");
 		replaceVariables(variablesManager, "${VAR1}zzz",    "VALUE1zzz" );
 		replaceVariables(variablesManager, "aaa${VAR1}",    "aaaVALUE1");
 		replaceVariables(variablesManager, "aaa${VAR1}zzz", "aaaVALUE1zzz");
 		
-		replaceVariables(variablesManager, "aaa${}zzz", "aaa${}zzz");
-		
+		// 2 variables in the original string		
 		replaceVariables(variablesManager, "aaa${VAR1}bbb${VAR2}zzz", "aaaVALUE1bbbVALUE2zzz");
+
+		// Multiple same variable in the original string
+		replaceVariables(variablesManager, "aaa${VAR1}bbb${VAR1}zzz", "aaaVALUE1bbbVALUE1zzz");
+		replaceVariables(variablesManager, "aaa${VAR1}bbb${VAR1}ccc${VAR2}zzz", "aaaVALUE1bbbVALUE1cccVALUE2zzz");
+	}
+	
+	public void testReplaceVarForTargetFolder() {
+// Example :
+//		# Entities_form
+//		Entity form.component.ts	; ${BEANNAME_LC}-form.component.ts		; ${SRC}/app/entities/${BEANNAME_LC}/${BEANNAME_LC}-form 	; src/app/entities/Entity_form/ENTITY-form_component_ts.vm		; *
+//		Entity form.component.html	; ${BEANNAME_LC}-form.component.html	; ${SRC}/app/entities/${BEANNAME_LC}/${BEANNAME_LC}-form 	; src/app/entities/Entity_form/ENTITY-form_component_html.vm	; *
+//		Entity form.component.css	; ${BEANNAME_LC}-form.component.css		; ${SRC}/app/entities/${BEANNAME_LC}/${BEANNAME_LC}-form 	; src/app/entities/Entity_form/ENTITY-form_component_css.vm     ; *
+		HashMap<String,String> hm = new HashMap<String,String>();
+		hm.put("${SRC}", "/foo/bar");
+		hm.put("${BEANNAME}", "Car");		
+		hm.put("${BEANNAME_LC}", "car");
+		hm.put("${BEANNAME_UC}", "CAR");
+		VariablesManager variablesManager = new VariablesManager(hm) ;
+		
+		replaceVariables(variablesManager, "${BEANNAME_LC}-form.component.ts", "car-form.component.ts");
+		replaceVariables(variablesManager, "${BEANNAME_UC}-form.component.ts", "CAR-form.component.ts");
+
+		replaceVariables(variablesManager, "${SRC}/app/entities/${BEANNAME_LC}/${BEANNAME_LC}-form", "/foo/bar/app/entities/car/car-form");
 	}
 	
 	public void testGetVariableNames() {
