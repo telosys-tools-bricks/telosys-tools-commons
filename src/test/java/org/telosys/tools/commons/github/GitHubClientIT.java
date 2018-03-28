2d94e4856cda18633d98ae54ef9d410cab967b3d
l-gu <laurent.guer1@gmail.com>
2018-03-28 16:18:56 +0200

    code cleaning

--- src/test/java/org/telosys/tools/commons/github/GitHubClientIT.java
+++ src/test/java/org/telosys/tools/commons/github/GitHubClientIT.java
@@ -22,36 +22,23 @@ public class GitHubClientIT extends TestCase {
 
 	private final static String GITHUB_USER = "telosys-tools" ;
 
-//	private final static String HTTPS_PROTOCOLS = "https.protocols" ;
-//	private final static String TLS_VER_1_2     = "TLSv1.2" ;
-//
-//	private void configTLSv2() {
-//		String httpsProtocols = System.getProperty(HTTPS_PROTOCOLS);
-//		if ( httpsProtocols != null && httpsProtocols.contains(TLS_VER_1_2) ) {
-//			// TLS v 1.2 is already set in the system property
-//			return ;
-//		}
-//		else {
-//			System.setProperty(HTTPS_PROTOCOLS, TLS_VER_1_2);
-//		}
-//	}
-	
 	private void printJavaVersion() {
 		System.out.println("Setting system property 'https.protocols'... ");
 
-		// OK    on testGetRepositories(), testGetRepositoriesWithoutProperties()
-		// Error on testDownloadRepository() : SSLException: Received fatal alert: protocol_version
-		//System.setProperty("https.protocols", "SSLv3,TLSv1,TLSv1.1,TLSv1.2"); 
+//		// OK    on testGetRepositories(), testGetRepositoriesWithoutProperties()
+//		// Error on testDownloadRepository() : SSLException: Received fatal alert: protocol_version
+//		//System.setProperty("https.protocols", "SSLv3,TLSv1,TLSv1.1,TLSv1.2"); 
+//		
+//		// System.setProperty("https.protocols", "SSLv3"); // 3 ERRORS
+//		
+//		//System.setProperty("https.protocols", "TLSv1.1,TLSv1.2"); // 2 OK, Error on testDownloadRepository()
+//		System.setProperty("https.protocols", "TLSv1.2"); // 2 OK, Error on testDownloadRepository()
+//		
+////		System.setProperty("deployment.security.SSLv2Hello", "false");
+////		System.setProperty("deployment.security.TLSv1.2", "true"); // 
 		
-		// System.setProperty("https.protocols", "SSLv3"); // 3 ERRORS
-		
-		//System.setProperty("https.protocols", "TLSv1.1,TLSv1.2"); // 2 OK, Error on testDownloadRepository()
-		System.setProperty("https.protocols", "TLSv1.2"); // 2 OK, Error on testDownloadRepository()
-		
-//		System.setProperty("deployment.security.SSLv2Hello", "false");
-//		System.setProperty("deployment.security.TLSv1.2", "true"); // 
-		
-		System.out.println("Java version : " + System.getProperty("java.runtime.version")); // eg : 1.6.0_45-b06
+		System.out.println("Java version    : " + System.getProperty("java.runtime.version")); // eg : 1.6.0_45-b06
+		System.out.println("Https protocols : " + System.getProperty("https.protocols"));
 	}
 
 	public void testGetRepositories() throws Exception  {
@@ -86,7 +73,6 @@ public class GitHubClientIT extends TestCase {
 
 		printJavaVersion() ;
 		
-		//Properties properties = HttpTestConfig.getSpecificProxyProperties();	
 		Properties properties = TestsEnv.loadSpecificProxyProperties() ;
 		GitHubClient gitHubClient = new GitHubClient( properties);
 		
