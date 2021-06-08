90cd8699588a6ef7c917468f5f3a8e6f2b7de6cd
l-gu <laurent@telosys.org>
2021-06-08 15:24:28 +0200

    IT test for http get GitHub api URL

--- src/test/java/org/telosys/tools/commons/http/HttpClientIT.java
+++ src/test/java/org/telosys/tools/commons/http/HttpClientIT.java
@@ -1,5 +1,6 @@
 package org.telosys.tools.commons.http;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,11 +19,12 @@ import junit.framework.TestCase;
  */
 public class HttpClientIT extends TestCase {
 
-	private final static String URL_GOOGLE_FR  = "https://www.google.fr" ;
-	private final static String URL_GITHUB_COM = "https://github.com/telosys-templates-v3" ;
+	private static final String URL_GOOGLE_FR  = "https://www.google.fr" ;
+	private static final String URL_GITHUB_COM = "https://github.com/telosys-templates-v3" ;
+	private static final String URL_GITHUB_API = "https://api.github.com/users/telosys-templates-v3/repos";
 	
 	private Map<String, String> getHeaders() {
-		Map<String, String> headers = new HashMap<String, String>();
+		Map<String, String> headers = new HashMap<>();
 		headers.put("Cache-Control",   "max-age=0");
 		headers.put("Accept-Encoding", "gzip,deflate");		
 		// headers.put("User-Agent",      "Telosys-HttpClient"); // Set by default
@@ -35,59 +37,92 @@ public class HttpClientIT extends TestCase {
 	
 	private void checkResponse(HttpResponse response, int expectedRetCode) {
 		assertEquals(expectedRetCode, response.getStatusCode());
-		System.out.println(" Ret Code = " +response.getStatusCode() );
+		println(" Ret Code = " +response.getStatusCode() );
 
 		
-		System.out.println("Header keys : ");
+		println("Header keys : ");
 		Map<String,List<String>> map = response.getHeaderMap();
 		for ( String key : map.keySet() ) {
-			System.out.println(" . '" + key + "'");
+			println(" . '" + key + "'");
 		}
-		System.out.println(" Header 'cache-control' : " + response.getHeader("Cache-Control") );
-		System.out.println(" Header 'content-encoding' : " + response.getHeader("Content-Encoding") );
-		System.out.println(" Header 'xx-yy-zz' : " + response.getHeader("xx-yy-zz") );
+		println(" Header 'cache-control' : " + response.getHeader("Cache-Control") );
+		println(" Header 'content-encoding' : " + response.getHeader("Content-Encoding") );
+		println(" Header 'xx-yy-zz' : " + response.getHeader("xx-yy-zz") );
 		
-		System.out.println(" ---------- " );
+		println(" ---------- " );
 	}
 	
+	private void println(String s) {
+		System.out.println(s);
+	}
+	private void printStatus(HttpResponse httpResponse) {
+		println("Status code : " + httpResponse.getStatusCode() );
+	}
+	private void printHeader(HttpResponse httpResponse, String headerName) {
+		println("Header '" + headerName + "' : " + httpResponse.getHeader(headerName) );
+	}
+
+	private void printBody(HttpResponse httpResponse) {
+		println("Body : " );
+		String body = new String(httpResponse.getBodyContent(), StandardCharsets.UTF_8);
+		println(body);
+	}
+	
+
 	//==========================================================================================
 
 	public void testGet1() throws Exception {
-		System.out.println("--- Test http GET 1 --- ");		
+		println("--- Test http GET 1 --- ");		
 		HttpClient httpClient = getHttpClient();
 		checkResponse(httpClient.get(URL_GOOGLE_FR, getHeaders()), 200);
 	}
 	
 	public void testGet2() throws Exception {
-		System.out.println("--- Test http GET 2 --- ");		
+		println("--- Test http GET 2 --- ");		
 		HttpClient httpClient = getHttpClient();
 		HttpRequest request = new HttpRequest(URL_GOOGLE_FR, getHeaders() );
 		checkResponse(httpClient.get(request), 200);
 	}
 
 	public void testGet3() throws Exception {
-		System.out.println("--- Test http GET 3 --- ");		
+		println("--- Test http GET 3 --- ");		
 		HttpClient httpClient = getHttpClient();
 		checkResponse(httpClient.get(URL_GITHUB_COM, getHeaders()), 200);
+	}
+	
+	public void testGetGitHubAPI() throws Exception {
+		println("--- Test http GET 3 --- ");		
+		HttpClient httpClient = getHttpClient();
+		HashMap<String,String> headers = new HashMap<>();
+		HttpResponse httpResponse = httpClient.get(URL_GITHUB_API, headers);
+		checkResponse(httpResponse, 200);
+		
+		printBody(httpResponse);
+		printStatus(httpResponse);
+		printHeader(httpResponse, "X-RateLimit-Limit");
+		printHeader(httpResponse, "X-RateLimit-Used");
+		printHeader(httpResponse, "X-RateLimit-Remaining");
+		printHeader(httpResponse, "X-RateLimit-Resource");
+		printHeader(httpResponse, "X-RateLimit-Reset");
 	}
 	
 	//==========================================================================================
 	
 	public void testHead1() throws Exception {
-		System.out.println("--- Test http HEAD 1 --- ");		
+		println("--- Test http HEAD 1 --- ");		
 		HttpClient httpClient = getHttpClient();
 		checkResponse(httpClient.head(URL_GOOGLE_FR, getHeaders()), 200);
 	}
 	
 	public void testHead2() throws Exception {
-		System.out.println("--- Test http HEAD 2 --- ");		
+		println("--- Test http HEAD 2 --- ");		
 		HttpClient httpClient = getHttpClient();		
 		HttpRequest request = new HttpRequest(URL_GOOGLE_FR, getHeaders() );
 		checkResponse(httpClient.head(request), 200);
 	}
 	
 	public void testHead3() throws Exception {
-		System.out.println("--- Test http HEAD 3 --- ");		
+		println("--- Test http HEAD 3 --- ");		
 		HttpClient httpClient = getHttpClient();
 		checkResponse(httpClient.head(URL_GITHUB_COM, getHeaders()), 200);
 	}
@@ -95,13 +130,13 @@ public class HttpClientIT extends TestCase {
 	//==========================================================================================
 	
 	public void testDelete1() throws Exception {
-		System.out.println("--- Test http DELETE 1 --- ");		
+		println("--- Test http DELETE 1 --- ");		
 		HttpClient httpClient = getHttpClient();
 		checkResponse(httpClient.delete(URL_GOOGLE_FR, getHeaders()), 405); // HTTP 405 : Method not allowed
 	}
 	
 	public void testDelete2() throws Exception {
-		System.out.println("--- Test http DELETE 2 --- ");		
+		println("--- Test http DELETE 2 --- ");		
 		HttpClient httpClient = getHttpClient();
 		HttpRequest request = new HttpRequest(URL_GOOGLE_FR, getHeaders() );
 		checkResponse(httpClient.delete(request), 405); // HTTP 405 : Method not allowed
@@ -109,26 +144,25 @@ public class HttpClientIT extends TestCase {
 	
 	//==========================================================================================
 	public void testDownload1() throws Exception {
-		System.out.println("Test DOWNLOAD 1 via HTTP ... ");
+		println("Test DOWNLOAD 1 via HTTP ... ");
 		
 		String urlString = "http://www.telosys.org/index.html";
 		String downloadFileName = TestsEnv.getTmpExistingFolderFullPath("download") + "/file1.tmp" ;
 
 		HttpClient httpClient = getHttpClient();
 		long r = httpClient.downloadFile(urlString, downloadFileName);
-		System.out.println("File " + urlString + " donwloaded (" + r + " bytes)");
+		println("File " + urlString + " donwloaded (" + r + " bytes)");
 	}
 
 	public void testDownload2() throws Exception {
-		System.out.println("Test DOWNLOAD 2 via HTTPS ... ");
+		println("Test DOWNLOAD 2 via HTTPS ... ");
 		
-		//String urlString = "https://api.github.com/users/telosys/repos" ;
 		String urlString = "http://www.telosys.org/index.html";
 		// NB : https can raise an error ( javax.net.ssl.SSLException ) => use http  for tests
 		String downloadFileName = TestsEnv.getTmpExistingFolderFullPath("download") + "/file2.tmp" ;
 
 		HttpClient httpClient = getHttpClient();
 		long r = httpClient.downloadFile(urlString, downloadFileName);
-		System.out.println("File " + urlString + " donwloaded (" + r + " bytes)");
+		println("File " + urlString + " donwloaded (" + r + " bytes)");
 	}
 }
